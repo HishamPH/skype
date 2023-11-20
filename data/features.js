9bbbbe71b2995335579ccad1961dbe0b7c2ad28c
HishamPH <110416566+HishamPH@users.noreply.github.com>
2023-11-20 09:54:20 +0530

    Add files via upload

---
+++ data/features.js
@@ -0,0 +1,25 @@
+export const features = [
+  {
+    image:`carousel1.webp`,
+    title:`20 years of Connecting Miles and Smiles`,
+    description:`Embrace a legacy of communication as Skype marks two decades of connecting hearts across borders to bring smiles and, enabling conversations that transcend miles. Join the global celebration of human connections that have shaped our world.`
+  },
+  {
+    image:`carousel2.webp`,
+    title:`​Want to be the hero on your next call? 🦸`,
+    description:`Did you know Skype Bing mashup happened? Here are the results - a unique high-quality background images in your Skype calls! And that's not all, you can now kill time by searching up the image and exploring more about the beautiful scenery and places you see on your background!`
+  },
+  {
+    image:`carousel3.webp`,
+    title:`Your chats are about to get much better, Skype's new and improved messaging is here!`,
+    description:`Last month we revealed our big plans for an improved, faster, reliable, and super modern-looking Skype. Now we're making good on our promise by rolling out the first of the new upgrades.`
+  },
+  {
+    image:`carousel4.webp`,
+    title:`Preserving the connections that matter most with Skype`,
+    description:`And thanks to Skype, it’s also never been easier. Here’s how people across the globe used technology to stay connected through it all.`
+  }
+]
+
+
+
